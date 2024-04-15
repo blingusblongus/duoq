369eeee22923de563dca022f364fee832f2f8da8
Nick Allen <rutherford.t.b@gmail.com>
2024-04-15 12:56:46 -0500

    init, add tailwind

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    tabWidth: 4,
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
