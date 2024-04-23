584e7d71eb37fb55dc9ab15fa065787507dafde4
Nick Allen <rutherford.t.b@gmail.com>
2024-04-23 10:49:40 -0500

    add expect-error

--- src/pages/api/dev-save-json.ts
+++ src/pages/api/dev-save-json.ts
@@ -13,6 +13,7 @@ export async function GET(context: APIContext) {
     }
 
     // Fetch Latest games
+    // @ts-expect-error - for some reason, even changing declarating file doesn't seem to be fixing locals typing
     if (!context.locals.user) {
         return new Response("No user session", { status: 401 });
     }
