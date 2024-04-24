7db4c315bed4d50efbcb3b9f523b95b7a0cb01f5
Nick Allen <rutherford.t.b@gmail.com>
2024-04-24 17:02:56 -0500

    refactor to handle up to 5 summoners

--- src/pages/api/duos/index.ts
+++ src/pages/api/duos/index.ts
@@ -2,40 +2,58 @@ import { riotService } from "@/services/RiotService";
 import type { APIContext } from "astro";
 import { sql, db, Summoner_Match } from "astro:db";
 
+const validateSummoner = async (fullName: FormDataEntryValue) => {
+    if (!fullName) throw Error("Summoner name is null");
+
+    if (typeof fullName !== "string") {
+        throw Error("Summoner name must be string");
+    }
+
+    const [gameName, tagline] = fullName.split("#");
+    const puuid = (await riotService.getSummonerByRiotId(gameName, tagline))
+        .puuid;
+    return puuid;
+};
 export async function POST(context: APIContext) {
     const form = await context.request.formData();
     const summoner1 = form.get("summoner1");
     const summoner2 = form.get("summoner2");
+    const summoner3 = form.get("summoner3");
+    const summoner4 = form.get("summoner4");
 
-    if (!summoner1)
-        return new Response("No summoner provided", { status: 400 });
+    // @ts-expect-error - puuid typing
+    const puuids = [context.locals.user.puuid];
 
-    if (typeof summoner1 !== "string") {
-        return new Response("Input must be a string", { status: 400 });
+    for (let s of [summoner1, summoner2, summoner3, summoner4]) {
+        if (!s) continue;
+        try {
+            puuids.push(await validateSummoner(s));
+        } catch (e) {
+            if (typeof e === "string") {
+                return new Response(e);
+            }
+            return new Response(
+                "Unknown error in duo endpoint:" + JSON.stringify(e),
+            );
+        }
     }
 
-    // @ts-expect-error - still haven't resolved this locals typing
-    const userPuuid = context.locals.user.puuid;
-
-    try {
-        const [gameName, tagline] = summoner1.split("#");
-        const summoner1Puuid = (
-            await riotService.getSummonerByRiotId(gameName, tagline)
-        ).puuid;
-
-        if (summoner2) {
-            if (!summoner2)
-                return new Response("No summoner provided", { status: 400 });
-
-            if (typeof summoner2 !== "string") {
-                return new Response("Input must be a string", { status: 400 });
-            }
-            const [gameName, tagline] = summoner2.split("#");
-            const summoner2Puuid = (
-                await riotService.getSummonerByRiotId(gameName, tagline)
-            ).puuid;
-            console.log(userPuuid, summoner1Puuid, summoner2Puuid);
-            const sq = sql`SELECT
+    let sq;
+    switch (puuids.length) {
+        case 2:
+            sq = sql`SELECT s1.teamPosition AS teamPosition1, s2.teamPosition AS teamPosition2, 
+                COUNT(*) AS games_played,
+                COUNT(*) FILTER (WHERE s1.win = true) AS games_won,
+                COUNT(*) FILTER (WHERE s1.win = false) AS games_lost,
+                COUNT(*) FILTER (WHERE s1.win = true) * 1.0 / COUNT(*) AS win_rate
+                FROM ${Summoner_Match} s1
+                JOIN ${Summoner_Match} s2 ON s1.matchId = s2.matchId AND s1.teamId = s2.teamId
+                WHERE s1.summonerId = ${puuids[0]} AND s2.summonerId = ${puuids[1]}
+                GROUP BY s1.teamPosition, s2.teamPosition
+                ORDER BY win_rate DESC`;
+            break;
+        case 3:
+            sq = sql`SELECT
                 s1.teamPosition AS teamPosition1,
                 s2.teamPosition AS teamPosition2,
                 s3.teamPosition AS teamPosition3,
@@ -46,27 +64,66 @@ export async function POST(context: APIContext) {
                 FROM ${Summoner_Match} s1
                 JOIN ${Summoner_Match} s2 ON s1.matchId = s2.matchId AND s1.teamId = s2.teamId
                 JOIN ${Summoner_Match} s3 ON s1.matchId = s3.matchId AND s1.teamId = s3.teamId
-                WHERE s1.summonerId = ${userPuuid} AND s2.summonerId = ${summoner1Puuid} AND s3.summonerId = ${summoner2Puuid}
+                WHERE 
+                    s1.summonerId = ${puuids[0]} AND 
+                    s2.summonerId = ${puuids[1]} AND 
+                    s3.summonerId = ${puuids[2]}
                 GROUP BY teamPosition1, teamPosition2, teamPosition3
                 ORDER BY win_rate DESC`;
-            const res = await db.run(sq).then((res) => res.rows);
-            return new Response(JSON.stringify(res));
-        } else {
-            const sq = sql`SELECT s1.teamPosition AS userPosition, s2.teamPosition AS friendPosition, 
+            break;
+        case 4:
+            sq = sql`SELECT
+                s1.teamPosition AS teamPosition1,
+                s2.teamPosition AS teamPosition2,
+                s3.teamPosition AS teamPosition3,
+                s4.teamPosition AS teamPosition4,
                 COUNT(*) AS games_played,
-                COUNT(*) FILTER (WHERE s1.win = true) AS games_won,
+                COUNT(*) FILTER (WHERE s1.win = true) AS games_won,  -- Assuming all are on the same team, if s1 wins, all win
                 COUNT(*) FILTER (WHERE s1.win = false) AS games_lost,
                 COUNT(*) FILTER (WHERE s1.win = true) * 1.0 / COUNT(*) AS win_rate
                 FROM ${Summoner_Match} s1
                 JOIN ${Summoner_Match} s2 ON s1.matchId = s2.matchId AND s1.teamId = s2.teamId
-                WHERE s1.summonerId = ${userPuuid} AND s2.summonerId = ${summoner1Puuid}
-                GROUP BY s1.teamPosition, s2.teamPosition
+                JOIN ${Summoner_Match} s3 ON s1.matchId = s3.matchId AND s1.teamId = s3.teamId
+                JOIN ${Summoner_Match} s4 ON s1.matchId = s4.matchId AND s1.teamId = s4.teamId
+                WHERE 
+                    s1.summonerId = ${puuids[0]} AND 
+                    s2.summonerId = ${puuids[1]} AND 
+                    s3.summonerId = ${puuids[2]} AND
+                    s4.summonerId = ${puuids[3]}
+                GROUP BY teamPosition1, teamPosition2, teamPosition3, teamPosition4
                 ORDER BY win_rate DESC`;
-
-            const res = await db.run(sq).then((res) => res.rows);
-            return new Response(JSON.stringify(res));
-        }
-    } catch (err) {
-        return new Response(JSON.stringify(err));
+            break;
+        case 5:
+            sq = sql`SELECT
+                s1.teamPosition AS teamPosition1,
+                s2.teamPosition AS teamPosition2,
+                s3.teamPosition AS teamPosition3,
+                s4.teamPosition AS teamPosition4,
+                s5.teamPosition AS teamPosition5,
+                COUNT(*) AS games_played,
+                COUNT(*) FILTER (WHERE s1.win = true) AS games_won,  -- Assuming all are on the same team, if s1 wins, all win
+                COUNT(*) FILTER (WHERE s1.win = false) AS games_lost,
+                COUNT(*) FILTER (WHERE s1.win = true) * 1.0 / COUNT(*) AS win_rate
+                FROM ${Summoner_Match} s1
+                JOIN ${Summoner_Match} s2 ON s1.matchId = s2.matchId AND s1.teamId = s2.teamId
+                JOIN ${Summoner_Match} s3 ON s1.matchId = s3.matchId AND s1.teamId = s3.teamId
+                JOIN ${Summoner_Match} s4 ON s1.matchId = s4.matchId AND s1.teamId = s4.teamId
+                JOIN ${Summoner_Match} s5 ON s1.matchId = s5.matchId AND s1.teamId = s5.teamId
+                WHERE 
+                    s1.summonerId = ${puuids[0]} AND 
+                    s2.summonerId = ${puuids[1]} AND 
+                    s3.summonerId = ${puuids[2]} AND
+                    s4.summonerId = ${puuids[3]} AND
+                    s5.summonerId = ${puuids[4]}
+                GROUP BY teamPosition1, teamPosition2, teamPosition3, teamPosition4, teamPosition5
+                ORDER BY win_rate DESC`;
+            break;
+        default:
+            return new Response("Unhandled puuids.length: " + puuids.length);
     }
+    console.time("lookup time");
+    const res = await db.run(sq).then((res) => res.rows);
+    console.timeEnd("lookup time");
+
+    return new Response(JSON.stringify(res));
 }
